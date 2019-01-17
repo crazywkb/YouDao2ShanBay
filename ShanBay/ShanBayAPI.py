532684aacb1772275c68a1009bff355371e534d9
crazyk <crazyk.wkb@gmail.com>
2019-01-18 01:44:55 +0800

    just complete ShanBay's login.

--- ShanBay/ShanBayAPI.py
+++ ShanBay/ShanBayAPI.py
@@ -2,22 +2,36 @@ import requests
 import dev_config
 import json
 
+config = dev_config
+
 
 class ShanBayAPI(object):
     def __init__(self):
         self.session = requests.Session()
 
+    def __change_session_headers(self, key, value):
+        self.session.headers.update({key: value})
+
     def login(self):
         __method = 'login'
+
+        self.__change_session_headers('content-type', 'application/json')
+
+        url = config.SHANBAY_API[__method]
+        post_data = json.dumps(config.SHANBAY_DATA[__method])
+
         try:
-            response = self.session.post(dev_config.SHANBAY_API[__method], data=json.dumps(dev_config.SHANBAY_DATA[__method]))
+            response = self.session.post(url=url, data=post_data)
             if response.status_code == 200:
-                print("Login Success.")
-            print(response.text)
+                return json.loads(response.text)
+            else:
+                return False
+
         except Exception as e:
             print(e)
+            return False
 
 
 if __name__ == '__main__':
     shanbay = ShanBayAPI()
-    shanbay.login()
+    print(shanbay.login())
