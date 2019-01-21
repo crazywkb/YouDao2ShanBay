79836345428aadd9df4ec729c43faee0f3ae2d59
crazywkb <crazyk.wkb@gmail.com>
2019-01-22 00:44:06 +0800

    add some configuration in config.py

--- config.py
+++ config.py
@@ -1,9 +1,41 @@
+from hashlib import md5
+
 YOUDAO_ACCOUNT_INFO = {
     'username': '',
     'password': ''
 }
 
 SHANBAY_ACCOUNT_INFO = {
-    'username': '',
+    'account': '',
     'password': ''
+}
+
+SHANBAY_API = {
+    'login': 'https://apiv3.shanbay.com/bayuser/login',
+    'add_vocabulary': 'https://www.shanbay.com/bdc/vocabulary/add/batch'
+}
+
+SHANBAY_DATA = {
+    'login': SHANBAY_ACCOUNT_INFO,
+    'add_vocabulary': {
+        'words': None,
+        # _: 'timestamp' optional
+    }
+}
+
+YOUDAO_API = {
+    'login': 'https://logindict.youdao.com/login/acc/login',
+    '': ''
+}
+
+YOUDAO_DATA = {
+    'login': {
+        'app': 'web',
+        'tp': 'urstoken',
+        'cf': 3,
+        'product': 'DICT',
+        'username': YOUDAO_ACCOUNT_INFO['username'],
+        'password': md5(YOUDAO_ACCOUNT_INFO['password'].encode()).hexdigest(),
+    },
+    '': ''
 }
